48bcd540b4b36df80b4bcd945138343fbd0c6d5f
gonas <143320324+gonas0919@users.noreply.github.com>
2025-03-21 10:25:53 +0900

    Update 과제 안 내신 분．．？.py

--- 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.py
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.py
@@ -1,8 +1,8 @@
-a = list()
-for i in range(1,31):
-    a.append(i)
-for p in range(1,29):
-    call = int(input())
-    a.remove(call)
-print(a[0])
-print(a[1])
+import sys
+input= sys.stdin.readline
+list1 = [i for i in range(1,31)]
+for i in range(28):
+    num = int(input().rstrip())
+    list1.remove(num)
+for i in list1:
+    print(i)

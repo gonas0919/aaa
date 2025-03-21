7b7cffba809ddbec22eca04d441bb2b65095aa7a
gonas <143320324+gonas0919@users.noreply.github.com>
2025-03-21 13:41:00 +0900

    Update 평균.py

--- 백준/Bronze/1546. 평균/평균.py
+++ 백준/Bronze/1546. 평균/평균.py
@@ -1,8 +1,9 @@
-N = int(input())
-l = list(map(int, input().split()))
-totalscore= 0
-maxscore = max(l)
-for i in range(len(l)):
-    l[i] = (l[i]/maxscore)*100
-    totalscore += l[i]
-print(totalscore/N)
+import sys
+input= sys.stdin.readline
+total = 0
+how = int(input().rstrip())
+list1 = list(map(int, input().rstrip().split()))
+newList = [x/max(list1)*100 for x in list1]
+for i in newList:
+    total += i
+print(total/how)

fc7b13d35fb4afa1e0e5fb62cc607b75a426d09a
gonas <143320324+gonas0919@users.noreply.github.com>
2025-03-21 13:12:04 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.py
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.py
@@ -1,12 +1,9 @@
-n,m = map(int, input().split())
-a = []
-a1 = []
-for b in range(1,n+1):
-    a.append(b)
-for p in range(m):
-    i , j = map(int, input().split())
-    a1 = a[i-1:j]
-    a1.reverse()
-    a[i-1:j] = a1
-for y in range(n):
-    print(a[y], end=' ')
+import sys
+input= sys.stdin.readline
+num, how = map(int, input().rstrip().split())
+list1 = [i for i in range(1,num+1)]
+for _ in range(how):
+    c1, c2 = map(int, input().rstrip().split()) # 1 2
+    list1[c1-1:c2] = list1[c1-1:c2][::-1] #0 1
+for i in list1:
+    print(i, end= ' ')  

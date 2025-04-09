7dfdfc6c8b83cc0cd9ea83992722c4b27bc8011a
gonas <143320324+gonas0919@users.noreply.github.com>
2025-04-09 20:16:27 +0900

    [Silver V] Title: 문서 검색, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1543. 문서 검색/문서 검색.py
+++ 백준/Silver/1543. 문서 검색/문서 검색.py
@@ -2,8 +2,4 @@ import sys
 input= sys.stdin.readline
 ask = input().rstrip()
 what = input().rstrip()
-how = 0
-while what in ask:
-    how += 1
-    ask = ask[ask.index(what)+len(what):]
-print(how)
+print(ask.count(what))

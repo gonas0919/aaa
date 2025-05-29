dca2c81dd59857d074f89aed7bc901599ee03970
gonas <143320324+gonas0919@users.noreply.github.com>
2025-05-30 01:17:19 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 1, Time: 552 ms, Memory: 71096 KB -BaekjoonHub

--- 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.py
+++ 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.py
@@ -1,26 +1,29 @@
 import sys
 sys.setrecursionlimit(10**6)
 input = sys.stdin.readline
-n , m ,r = map(int, input().rstrip().split())
-graph = [[] for i in range(n+1)]
-visited = [False] * (n+1)
-for i in range(1,m+1):
-    u,v = map(int, input().rstrip().split())
-    graph[u].append(v)
-    graph[v].append(u)
-result = [0] * (n+1)
-for i in range(1,n+1):
+
+n,m,k = map(int, input().rstrip().split())
+visited = [False]*(n+1)
+graph = [[] for _ in range(n+1)]
+result = [0]*(n+1)
+for i in range(m): #6번
+    x,z = map(int, input().rstrip().split())
+    graph[x].append(z)
+    graph[z].append(x)
+for i in range(n+1):
     graph[i].sort()
 a = 1
-def dfs(graph, v, visited):
-    visited[v] = True
+def dfs(start):
+    visited[start] = True
     global a
-    result[v] += a
-    a +=1
-    for j in graph[v]:
-        if not visited[j]:
-            dfs(graph, j , visited)
+    result[start] = a
+    a += 1
+    
+    for i in graph[start]:    
+        if not visited[i]: 
+            dfs(i)
 
-dfs(graph,r,visited)
+
+dfs(k)
 for i in result[1:]:
     print(i)

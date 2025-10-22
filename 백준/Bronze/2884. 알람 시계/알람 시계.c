c5c9dea1dd5054e5c9a7ef2de602f34871894e45
gonas <143320324+gonas0919@users.noreply.github.com>
2025-10-22 20:35:15 +0900

    [Bronze III] Title: 알람 시계, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2884. 알람 시계/알람 시계.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int H,M;
+    scanf("%d %d", &H,&M);
+    if (M-45<0){
+        M = 60 + M - 45;
+        H -= 1;
+        if (H<0){
+            H = 24 + H;
+        };
+    } else {M -= 45;}
+    printf("%d %d", H,M);
+    return 0;
+}
